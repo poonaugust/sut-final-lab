e53bf922c58752f51515f7592dc17dc93761145b
poonaugust <poonaugust1007@gmail.com>
2026-01-07 15:33:45 +0700

    Test Task2 - close #2

--- backend/entity/employee.go
+++ backend/entity/employee.go
@@ -9,5 +9,5 @@ type Employees struct {
 
 	Name   string  `valid:"stringlength(2|80)"`
 	Salary float64 `valid:"range(15000|200000)~Salary must be between 15000 and 200000"`
-	//EmployeeCode string `valid:"matches(^[A-Z])`
+	EmployeeCode string `valid:"matches(^[A-Z]{2}-[0-9]{4}$)~EmployeeCode mustbe 2 uppercase English letters (A-Z) followed by '-' and 4 digits (0-9)"`
 }
